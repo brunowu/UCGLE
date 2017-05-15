9f31ad9ba7b453bb874627b6211a0296965e8d55
brunowu <xinzhe.wu1990@gmail.com>
2017-05-16 00:05:13 +0200

    change main.c to petsc-3.7

--- main.c
+++ main.c
@@ -1,24 +1,4 @@
-/*
- This file is part of software for the implementation of UCGLE method, under the supervision of Serge G. Petiton
- <serge.petiton@univ-lille1.fr>.
- 
- Copyright (C) 2011—. Pierre-Yves AQUILANTI and Xinzhe WU <xinzhe.wu@ed.univ-lille1.fr> in Maison de la Simulation. 
- All rights reserved.
- 
- Permission to use, copy, modify and distribute this software for personal and educational use is hereby granted
- without fee, provided that the above copyright notice appears in all copies and that both that copyright notice 
- and this permission notice appear in supporting documentation, and that the names of all authors are not used in 
- advertising or publicity pertaining to distribution of the software without specific, written prior permission. 
- Xinzhe WU and the author make no representations about the suitability of this software for any purpose. It is 
- provided "as is" without express or implied warranty.
- 
- You should have received a copy of the GNU Lesser General Public License along with UCGLE.  If not, see 
- <http://www.gnu.org/licenses/>.
-
- For more information, contact with Xinzhe WU <xinzhe.wu@ed.univ-lille1.fr>.
- 
- */
-
+/*Copyright (c) 2011—2016. Pierre-Yves AQUILANTI and Xinzhe WU in Maison de la Simulation. All rights reserved */
 #include "main.h"
 
 static const char help[] = "Solve";
@@ -38,10 +18,16 @@ int main(int argc, char ** argv){
 	char cCurrentPath[FILENAME_MAX];
 	char path[FILENAME_MAX];
 	
+	time_t firstTime, secondTime;	
 	mode_t process_mask = umask(0);
 	/* init of MPI and MPI Utils */
 	MPI_Init(&argc,&argv);
+
+///	com.com_world=MPI_COMM_WORLD;
+
 	mpi_lsa_init(argc,argv,&com);
+
+///	com.com_world=MPI_COMM_WORLD;
 
 	MPI_Barrier(MPI_COMM_WORLD);
 
@@ -49,19 +35,76 @@ int main(int argc, char ** argv){
 	PETSC_COMM_WORLD=com.com_group; // give group communicator as main petsc communicator
 	ierr=SlepcInitialize(&argc,&argv,(char *)0,help);	CHKERRQ(ierr);
 
-	/*	VecCreate()*/
+//	PetscPrintf(com.com_world,"]> Initializing PETSc/SLEPc\n");
+
+/*	VecCreate()*/
 
 	/* programm options */
 	ierr=PetscOptionsBegin(PETSC_COMM_WORLD, "", "Options for the GMRES/LS-Arnoldi solver", "KSP");CHKERRQ(ierr);
 
-	/* now read data files */
+	/* create temp directories */
+
+//	PetscPrintf(com.com_world,"]> Checking temporary directory structure\n");
+
+	/* See if the user choosed a tmp directory in order to unpack the matrix file for each node group */
+	ierr=PetscOptionsGetString(NULL,PETSC_NULL,"-tmp_path",tmp_path,PETSC_MAX_PATH_LEN-1,&flag);CHKERRQ(ierr);
+	if (!flag) {
+		sprintf(tmp_path,"matrices_tmp");
+	}
+
+
+	/* Get the directory name */
+ 	if (!getcwd(cCurrentPath, sizeof(cCurrentPath))){
+			SETERRQ(PETSC_COMM_WORLD,(PetscErrorCode) 83,"cannot write\n");
+  }
+	/* Create the directory for temp files */
+	sprintf(path,"%s/%s",cCurrentPath,tmp_path);
+	result_code = mkdir(path, S_IRWXU | S_IRWXG | S_IRWXO);
+	umask(process_mask);
+
+
+	/* Now create a temp directory for each group */
 	MPI_Barrier(MPI_COMM_WORLD);
-	PetscPrintf(com.com_world,"]> Opening input data files\n");
+
+//	PetscPrintf(com.com_world,"]> Creating directory : %s\n]> Creating the directory structure for each group...\n",path);
+
+	sprintf(tmp_path,"%s",path);
+	MPI_Barrier(MPI_COMM_WORLD);
+
+
+	/* get the names */
+	if(com.color_group==GMRES_GR){
+		sprintf(path,"%s/gmres",tmp_path);
+	}else if(com.color_group==FATHER_GR){
+		sprintf(path,"%s/father",tmp_path);
+	}else if(com.color_group==ARNOLDI_GR){
+		sprintf(path,"%s/arnoldi",tmp_path);
+	}else if(com.color_group==LS_GR){
+		sprintf(path,"%s/ls",tmp_path);
+	}
+
+	/* set permissions */
+
+//	PetscPrintf(com.com_group,"]> Creating directory : %s\n",path);
+
+	result_code = mkdir(path, S_IRWXU | S_IRWXG | S_IRWXO);
+	umask(process_mask);
+	setenv("PETSC_TMP",path,1);
+
+	/* now read data files */
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+//	PetscPrintf(com.com_world,"]> Opening input data files\n");
+
 	MPI_Barrier(MPI_COMM_WORLD);
 	
+	//ierr = MatrCeate(MPI_Comm*) &com.com_world, &A);CHKERRQ(ierr);	// creating Mat A before loading it from the file 
+	//ierr = MatSetFromOptions(A);CHKERRQ(ierr);
 	read_matrix_vector(&A, &v,(MPI_Comm*) &com.com_world);
 	Af=A;
 	vf=v;
+
 
 	PetscOptionsName("-Aclx","Convert complex matrix to real","PetscAclx",&flag);
 	if (flag) {
@@ -72,29 +115,66 @@ int main(int argc, char ** argv){
 	A=Af;
 	vf=v;
 
+
 	ierr=PetscOptionsEnd();CHKERRQ(ierr);
 
 	/* now that we got the data, groups will exchange information on data repartition */
+
+//	PetscPrintf(com.com_world,"]> Now exchanging vector sizes...\n");
+
 	mpi_lsa_com_vecsize_init(&com,&v);
 
 	MPI_Barrier(MPI_COMM_WORLD);
-	ierr=PetscOptionsGetInt(PETSC_NULL,"-ksp_ls_nopc",&nols,&flag);CHKERRQ(ierr);
+
+///	PetscPrintf(com.com_world,"]> Vector Exchange OK   ...\n");
+
+	ierr=PetscOptionsGetInt(NULL,PETSC_NULL,"-ksp_ls_nopc",&nols,&flag);CHKERRQ(ierr);
 	if(!flag) nols=1;
 	else if(nols==0) PetscPrintf(com.com_world,"]> not using LSA preconditionning %d\n",nols);
+ 	time(&firstTime);
+
+	/*create viewers for each group if asked for*/
+	PetscOptionsHasName(NULL,PETSC_NULL,"-output_file",&flag);
+	if(flag){
+	      if(com.color_group==GMRES_GR){
+		      //PetscPrintf(com.com_group,"]> Launching GMRES\n");
+		      launchGMRES(&com, &v, & A);
+	      }else if(com.color_group==ARNOLDI_GR && nols!=0){
+		      //PetscPrintf(com.com_group,"]> Launching Arnoldi\n");
+		      Arnoldi(&com,&A,&v)	;
+	      }	else if(com.color_group==FATHER_GR && nols!=0){
+		      //PetscPrintf(com.com_group,"]> Launching Father\n");
+		      Father(&com,&v);
+	      } else if(com.color_group==LS_GR && nols!=0){
+		      VecGetSize(v,&vsize);
+		      //PetscPrintf(com.com_group,"]> Launching LS\n");
+		      LSQR(&com,&vsize);
+	      }
+	}
+
+
 
 	if(com.color_group==GMRES_GR){
-	                PetscPrintf(com.com_group,"]> Launched GMRES\n");
+		PetscPrintf(com.com_group,"]> Launching GMRES\n");
 		launchGMRES(&com, &v, &A);
-	//	PetscPrintf(com.com_group,"]> Launched GMRES\n");
-
 	}else if(com.color_group==ARNOLDI_GR && nols!=0){
+		PetscPrintf(com.com_group,"]> Launching Arnoldi\n");
 		Arnoldi(&com,&A,&v)	;
 	}	else if(com.color_group==FATHER_GR && nols!=0){
+		PetscPrintf(com.com_group,"]> Launching Father\n");
 		Father(&com,&v);
 	} else if(com.color_group==LS_GR && nols!=0){
 		VecGetSize(v,&vsize);
+		PetscPrintf(com.com_group,"]> Launching LS\n");
 		LSQR(&com,&vsize);
 	}
+
+	time(&secondTime);
+
+	//PetscPrintf(PETSC_COMM_WORLD,"End of Computation\n");
+
+	PetscPrintf(PETSC_COMM_WORLD," TOTAL ELLAPSED TIME = %f seconds", difftime(secondTime,firstTime));
+
 
 	/* end of the programm, deallocate structures and arrays */
  	MPI_Barrier(MPI_COMM_WORLD);
